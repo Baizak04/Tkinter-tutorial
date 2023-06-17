236bade8f14ed770eb58a02cffab8d0bc5e86c49
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-17 22:06:48 +0600

    basic class Window

--- tkinter2/window.py
+++ tkinter2/window.py
@@ -1,8 +1,20 @@
 from tkinter import *
 
-window = Tk()
-window.title('Tkinter example')
-window.geometry('400x500+600+300')
-window.resizable(False, False)
 
-window.mainloop()
+class Window:
+    def __init__(self, widht, height, title="MyWindow", resizable=(False, False), icon=None):
+        self.root = Tk()
+        self.root.title(title)
+        self.root.geometry(f"{widht}x{height}+200+200")
+        self.root.resizable([0], resizable[1])
+        if icon:
+            self.root.iconbitmap(icon)
+            
+    
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    window = Window(500, 500, "TKINTER")
+    window.run()
