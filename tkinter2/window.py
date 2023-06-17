d04b5f30fffa2eb7f74d3e626c720946f292e102
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-06-17 22:24:32 +0600

    new file child_window.py

--- tkinter2/window.py
+++ tkinter2/window.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from child_window import ChildWindow
 
 class Window:
     def __init__(self, widht, height, title="MyWindow", resizable=(False, False), icon=None):
@@ -14,7 +14,11 @@ class Window:
     def run(self):
         self.root.mainloop()
 
+    
+    def create_child(self, widht, height, title="Child", resizable=(False, False), icon=None):
+        ChildWindow(self.root, widht, height, title, resizable, icon)
 
 if __name__ == "__main__":
     window = Window(500, 500, "TKINTER")
+    window.create_child(200, 100)
     window.run()
